885f618533133c525e7a22825c5a8e625a8431ee
COMFORT CHIDI <rayspeedemails@gmail.com>
2023-08-01 14:57:53 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-let links = document.querySelectorAll('nav a');
+//function to highlight current menu on click
+function highlightCurrentURL() {
+    var a = document.getElementById("navig").getElementsByTagName('a');
+    for (var i = 0; i < a.length; i++) {
+        if (a[i].href.split("#")[0] == document.location.href.split('#')[0]) {
+            a[i].className = "current";
+        }
+    }
+}
 
-links.forEach(a => {
-a.addEventListener('click', function(){
-    links.forEach(btn => btn.classList.remove('active'));
-    this.classList.add('active');
+window.onload = function() {
+    highlightCurrentURL();
+}
+
+
+//mobile-nav-toggle Hamburger Menu
+const primaryNav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click", () => {
+    const visibility = primaryNav.getAttribute("data-visible");
+    
+    if(visibility === "false") {
+        primaryNav.setAttribute("data-visible", true);
+        navToggle.setAttribute("aria-expanded", true);
+    }
+    else if (visibility === "true") {
+        primaryNav.setAttribute("data-visible", false);
+        navToggle.setAttribute("aria-expanded", false);
+    }
 });
-});
-
-
 
